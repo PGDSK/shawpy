aa061fbac7e257a056f2b0eef99b8b14da4c799f
PGDSK <ethansbrayshaw@gmail.com>
2026-08-18 00:33:42 +1000

    KDE script change

--- 03_scripts/gaming.sh
+++ 03_scripts/gaming.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-qdbus org.kde.ActivityManager /ActivityManager/Activities \
-org.kde.ActivityManager.Activities.SetCurrentActivity \
-2377149b-8e23-4537-9ae2-6d49e1854547
+qdbus org.kde.ActivityManager /ActivityManager/Activities org.kde.ActivityManager.Activities.SetCurrentActivity 55420184-615d-4d8f-ad23-5f70aad9669c
 
-powerprofilesctl set balanced
+powerprofilesctl set performance
 
-kscreen-doctor \
-    output.1.brightness.80 \
-    output.2.brightness.80 \
-    output.3.brightness.80
+kscreen-doctor output.1.brightness.100 output.2.brightness.100 output.3.brightness.100
 
 night=$(qdbus org.kde.KWin /org/kde/KWin/NightLight org.kde.KWin.NightLight.running)
 
 if [ "$night" = "true" ]; then
-    qdbus org.kde.kglobalaccel /component/kwin \
-        org.kde.kglobalaccel.Component.invokeShortcut "Toggle Night Color"
+    qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut "Toggle Night Color"
 fi
