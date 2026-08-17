fbb0cc92610eea6c59a64ce7beca03acea88e548
PGDSK <ethansbrayshaw@gmail.com>
2026-08-18 00:16:47 +1000

    KDE script changes

--- 03_scripts/chill.sh
+++ 03_scripts/chill.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+qdbus org.kde.ActivityManager /ActivityManager/Activities \
+org.kde.ActivityManager.Activities.SetCurrentActivity \
+284c05c2-88db-47bc-bf80-627d1610ebba
+
 powerprofilesctl set power-saver
 
 kscreen-doctor \
@@ -7,4 +11,9 @@ kscreen-doctor \
     output.2.brightness.40 \
     output.3.brightness.40
 
+night=$(qdbus org.kde.KWin /org/kde/KWin/NightLight org.kde.KWin.NightLight.running)
 
+if [ "$night" = "false" ]; then
+    qdbus org.kde.kglobalaccel /component/kwin \
+        org.kde.kglobalaccel.Component.invokeShortcut "Toggle Night Color"
+fi
